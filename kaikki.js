9ca73c8613c959202a981116a14e2f3f2cd69664
joonaun <joonas.niemi@metropolia.fi>
2020-01-20 10:54:37 +0200

    Javascript tehtäviä

--- kaikki.js
+++ kaikki.js
@@ -1,3 +1,17 @@
 function folders() {
-    
+    let myUrl = "https://raw.githubusercontent.com/jopakka/Web-tekniikat_javascript/master/tehtavat.json?token=AGNGJDS65ET7NDZTUXBGC4K6EVUMK";
+
+    fetch(myUrl)
+        .then(function(vastaus){
+            return vastaus.json();
+        }).then(function(json){
+        naytaKuva(json);
+    }).catch(function(error){
+        console.log(error);
+    });
+}
+
+function naytaKuva(vuorovaikuttteiset) {
+    let x = vuorovaikuttteiset[0];
+    console.log(x);
 }
