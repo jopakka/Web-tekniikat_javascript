695b6bbd426c5b1bfc518b29cd8dda2ef16d3c56
joonaun <joonas.niemi@metropolia.fi>
2020-01-21 09:54:32 +0200

    Javascript tehtäviä

---
+++ dom/tehtava2/teht2.js
@@ -0,0 +1,5 @@
+//Muokkaa teht2.js tiedostoa ja tee skripti, joka tuo kuvan näkyviin style attribuuttia muokkaamalla
+document.getElementsByTagName("img")[0].style = "display: inline";
+
+//Muokkaa teht2.js tiedostoa ja tee skripti, joka muokkaa <img> elementin class attribuuttia siten, että kuva tulee näkyviin
+document.getElementsByClassName("hidden")[0].className = "showImg";
